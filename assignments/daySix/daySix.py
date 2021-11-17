5972fadc0639fcc3e18a08bdec2fd57e40eb8de7
joshua-davis1 <joshua.davis@smoothstack.com>
2021-11-17 09:20:24 -0500

    added daySix to assignments

---
+++ assignments/daySix/daySix.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def two():
+    np.zeros(10)
+
+def three():
+    np.ones(10)
+
+def four():
+    np.full(1,10)
+
+def five():
+    np.arange(10,51)
+
+def six():
+    np.arange(10,51,2)
+
+def seven():
+    np.arange(0,9).reshape(3,3)
+
+def eight():
+    np.identity(3)
+
+def nine():
+    np.random.randint(0,1)
